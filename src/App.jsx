a2e1415fdf9e1998cca8f53a2cffa73b95e0009c
Camilla Estevao <SEUEMAIL@gmail.com>
2026-06-25 10:54:46 -0300

    Corrige rotas com HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Inventario from "./pages/Inventario";
@@ -7,14 +7,14 @@ import Relatorios from "./pages/Relatorios";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/inventario" element={<Inventario />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/relatorios" element={<Relatorios />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
